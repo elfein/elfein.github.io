168dc22ead0d64f4a8d3957272b89f1f19752cfb
elfein <elfein99@gmail.com>
2018-10-23 21:58:14 -0400

    Update header style, add minor project descriptions

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const images = ['https://images.unsplash.com/photo-1474226004578-62743874270f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c74f092b48fff23b65fbdebe04613f61&auto=format&fit=crop&w=1950&q=80','https://images.unsplash.com/photo-1508669232496-137b159c1cdb?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5e05e923eadbc20a00da3a1a441dd3e3&auto=format&fit=crop&w=2734&q=80', 'https://images.unsplash.com/photo-1534832796130-37cfe118e925?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=83fca2459a46db84a04e807aadd0c38c&auto=format&fit=crop&w=2249&q=80']
+
 $(function () {
     console.log('present')
 
